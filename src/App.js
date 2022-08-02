4abaf0054d178e910b557c67819a8fd9cfdb6879
mugambi645 <mugambi645@gmail.com>
2022-08-02 21:46:32 +0300

    features component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/home/Navbar';
 import Hero from './components/home/Hero';
+import Features from './components/home/Features';
 function App() {
   return (
     <div className="App">
       <Navbar/>
      <Hero/>
+     <Features/>
     </div>
   );
 }
