3d34fb865a38fbb03597c541a08a45f915413437
mugambi645 <mugambi645@gmail.com>
2022-08-02 21:54:55 +0300

    productive component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Navbar from './components/home/Navbar';
 import Hero from './components/home/Hero';
 import Features from './components/home/Features';
+import Productive from './components/home/Productive';
 function App() {
   return (
     <div className="App">
       <Navbar/>
      <Hero/>
      <Features/>
+     <Productive/>
     </div>
   );
 }
